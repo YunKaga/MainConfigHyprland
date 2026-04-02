a4bdc8864c5a774d471560c8d84fb53ab7faea24
YunKaga <voronzowyura@gmail.com>
2026-04-03 00:34:26 +0700

    script

---
+++ after.sh
@@ -0,0 +1,23 @@
+#!/bin/zsh
+
+setfont cyr-sun16
+
+sudo systemctl enable sddm
+
+mkdir ~/gitclone/ 2> /dev/null
+cd ~/gitclone/
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+yay -Sy spotify zoom
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sudo cp -r /usr/share/zsh/plugins/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/
+sudo cp -r /usr/share/zsh/plugins/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+
+rm ~/.zshrc 2> /dev/null
+cp ~/.config/.zshrc ~/
+
+rm ~/.bashrc ~/.bash_logout ~/.bash_profile
